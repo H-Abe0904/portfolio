2ce6e327d86bd7495a181575e1d1a0c5d1024c4c
H-Abe0904 <113865937+H-Abe0904@users.noreply.github.com>
2026-03-06 17:50:09 +0900

    sss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+
+// Astroの設定ファイル(現状はTailwind CSSをViteに統合するための設定のみ)
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
