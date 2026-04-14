801f104b70b282aa8f65b01ccfc3acc49cef42e9
H-Abe0904 <113865937+H-Abe0904@users.noreply.github.com>
2026-04-15 00:02:07 +0900

    yeah

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 
 // Astroの設定ファイル(現状はTailwind CSSをViteに統合するための設定のみ)
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [mdx()],
 });
---
+++ src/content/blog/firstnote.md
@@ -0,0 +1,10 @@
+---
+layout: ../../layouts/MarkdownPostLayout.astro
+title: "1発目"
+pubDate: 2025-04-14
+description: "ご挨拶"
+author: "lusteria"
+tags: ["Astro", "ご挨拶"]
+---
+
+##  はじめまして
